677730c75316a20f9a55e35ad5cb25bf7ceae8bc
mana jalili <jalilimana@gmail.com>
2020-03-24 15:29:25 +0100

    added the js and bootstarp and gems

--- app/controllers/activities_controller.rb
+++
@@ -1,5 +0,0 @@
-class ActivitiesController < ApplicationController
-  def index
-    @activities = Activity.all
-  end
-end
--- db/migrate/20200324140601_remove_duration_from_activities.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDurationFromActivities < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :activities, :duration, :integer
-  end
-end
