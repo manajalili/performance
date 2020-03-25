82703d9e9771926d096722246dc245c0b827b2ca
mana jalili <jalilimana@gmail.com>
2020-03-25 16:39:39 +0100

    added the index method and it's view

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_24_143054) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "activities", force: :cascade do |t|
     t.string "name"
     t.text "description"
