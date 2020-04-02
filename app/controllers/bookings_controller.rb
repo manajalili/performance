9bc4f02b3dccaf5949a2dfd0b6acd330a626f000
mana jalili <jalilimana@gmail.com>
2020-04-02 21:35:38 +0200

    added the activity button and with th route

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,22 @@
 class BookingsController < ApplicationController
   def new
+    @day = Day.find(params[:day_id])
     @booking = Booking.new
   end
 
   def create
-    @booking = Booking.create(:bookingparams)
+    @booking = Booking.new(:bookingparams)
+    @day = Day.find(params[:day_id])
+    @booking.day = @day
     if @booking.save
       redirect_to day_path(@day)
     else
       render :new
     end
+  end
+
+  private
+  def bookingparams
+    params.require(:booking).permit(:duration, :activity_id)
   end
 end
