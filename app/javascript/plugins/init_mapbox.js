8cd0cae2f4f4f7303e1651a7cf7d789b05fd8a2a
mana jalili <jalilimana@gmail.com>
2020-04-05 16:10:59 +0200

    added the css component

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -36,3 +36,14 @@ if (mapElement) {
   // [ ... ]
   fitMapToMarkers(map, markers);
 }
+
+const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup) // add this
+      .addTo(map);
+  });
+};
