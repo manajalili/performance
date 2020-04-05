e9c1077af72b8b503212fb7d6545029e04d750e4
mana jalili <jalilimana@gmail.com>
2020-04-05 16:12:44 +0200

    done wiht geocode setup

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -47,3 +47,12 @@ const addMarkersToMap = (map, markers) => {
       .addTo(map);
   });
 };
+
+
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+// [...]
+if (mapElement) {
+  // [...]
+  map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+                                      mapboxgl: mapboxgl }));
+}
