f6dc8060669b072c1b5350d3a9cd8411fa4c2b1b
mana jalili <jalilimana@gmail.com>
2020-04-08 19:23:49 +0200

    flatpicker is working properly

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,7 +18,7 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import "../plugins/flatpickr"
-import { initFlatpickr } from "../plugins/flatpickr";
+import "../plugins/flatpickr";
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import { initDetectLocation } from '../plugins/init_detectlocation';
