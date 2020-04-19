6485689fb2d4c34f46da0bf81e092a90c8f0f271
mana jalili <jalilimana@gmail.com>
2020-04-19 15:25:51 +0200

    changed the weather from standard to metric unit

--- app/javascript/plugins/weather.js
+++ app/javascript/plugins/weather.js
@@ -5,18 +5,21 @@ const image = document.querySelector("#image");
 const location = document.querySelector("#header1");
 const weather = document.querySelector("#header4");
 const temprature = document.querySelector("#parag");
+const sunrise = document.querySelector("#sunrise");
+const sunset = document.querySelector("#sunset");
 
 form.addEventListener("submit", (e) => {
   console.log(city.value);
   e.preventDefault();
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=65a96098db32030e683e25c088180ad1`)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=metric&appid=65a96098db32030e683e25c088180ad1`)
     .then(response => response.json())
     .then((data) => {
       console.log(data);
 
       location.innerText = data.name;
-      weather.innerText = data.weather[0].main;
+      weather.innerText = data.weather[0].description;
       temprature.innerText = data.main.temp;
       image.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      sunrise.innerText = data.sys[3].sunrise;
     });
 });
