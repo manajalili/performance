03254e3d43fe575cadedeee2b24d4cc9b5a8ab8f
mana jalili <jalilimana@gmail.com>
2020-04-20 13:20:39 +0200

    added the js codes

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,69 +1,33 @@
 import mapboxgl from 'mapbox-gl';
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
-const mapElement = document.getElementById('map');
 
-const buildMap = () => {
-  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-  return new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/manajll/ck7owfbsy15r51iplhcp9h77y'
-  });
-};
+const location = document.querySelector('.list-unstyled');
+const form = document.querySelector("#location");
+const address = document.querySelector("#address");
 
-const addMarkersToMap = (map, markers) => {
-  markers.forEach((marker) => {
-    new mapboxgl.Marker({ color: "#fcb7a6" })
-      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+form.addEventListener("submit", (e) => {
+  console.log(address.value);
+  e.preventDefault();
+  location.innerHTML = '';
+  fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${address.value}.json?access_token=pk.eyJ1IjoibWFuYWpsbCIsImEiOiJjazZrbG1sZXUwMGhhM2RsYWU4emZkMjAxIn0.2L-RxLkhWL7pGw4IqnL6XA`)
+    .then(response => response.json())
+    .then((data) => {
+      const coordinates = `<li>${data.features[0].geometry.coordinates}</li>`;
+      const longitude = data.features[0].geometry.coordinates[0];
+      const latitude = data.features[0].geometry.coordinates[1];
+      location.insertAdjacentHTML("beforeend", coordinates);
 
-      new mapboxgl.Marker({ color: "#fcb7a6" })
-        .setLngLat([ marker.lng, marker.lat ])
-        .setPopup(popup) // add this
-        .addTo(map);
-  });
-};
+      mapboxgl.accessToken = 'pk.eyJ1IjoibWFuYWpsbCIsImEiOiJjazZrbG1sZXUwMGhhM2RsYWU4emZkMjAxIn0.2L-RxLkhWL7pGw4IqnL6XA';
+      const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        center: [longitude, latitude],
+        zoom: 12
+      });
 
-const fitMapToMarkers = (map, markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 100, duration: 2500 });
-};
-
-const initMapbox = () => { //this is the default from mapbox
-  if (mapElement) {
-    const map = buildMap();
-    const markers = JSON.parse(mapElement.dataset.markers);
-    addMarkersToMap(map, markers);
-    fitMapToMarkers(map, markers);
-
-    markers.forEach((marker) => {
-
-      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
-
-      // Create a HTML element for your custom marker
-      const element = document.createElement('div');
-      element.className = 'marker';
-      element.style.backgroundImage = `url('${marker.image_url}')`;
-      element.style.backgroundSize = 'contain';
-      element.style.width = '25px';
-      element.style.height = '25px';
-
-      // Pass the element as an argument to the new marker
-      new mapboxgl.Marker(element)
-        .setLngLat([marker.lng, marker.lat])
-        .setPopup(popup)
+      new mapboxgl.Marker()
+        .setLngLat([longitude, latitude])
         .addTo(map);
     });
-
-    map.addControl(
-      new mapboxgl.GeolocateControl({
-      positionOptions: {
-      enableHighAccuracy: true
-      },
-      trackUserLocation: true
-      })
-      );
-  }
-};
-
+});
 export { initMapbox };
